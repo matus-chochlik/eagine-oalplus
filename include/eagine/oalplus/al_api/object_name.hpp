da8ab5800f07cdc58fe1d1c9802dd4488a6a6368
Matus Chochlik <chochlik@gmail.com>
2022-05-27 07:29:10 +0200

    Multiple changes
    - Added new aliases for handle containers.
    - Added the generic clean_up functions for objects and object containers.
    - Updated the core sub-module.
    - Documentation updates.

--- include/eagine/oalplus/al_api/object_name.hpp
+++ include/eagine/oalplus/al_api/object_name.hpp
@@ -14,12 +14,51 @@
 
 namespace eagine::oalplus {
 //------------------------------------------------------------------------------
+/// @brief Alias for template wrapping GL object handles.
+/// @ingroup gl_api_wrap
+/// @see gl_owned_object_name
+/// @see al_object_name_view
+/// @see al_object_name_span
+/// @see al_object_name_array
 template <typename Tag>
 using al_object_name = c_api::basic_handle<Tag, al_types::name_type>;
 
+/// @brief Alias for template wrapping owned GL object handles.
+/// @ingroup gl_api_wrap
+/// @see al_object_name
 template <typename Tag>
 using al_owned_object_name =
   c_api::basic_owned_handle<Tag, al_types::name_type>;
+
+/// @brief Alias for template wrapping a mutable span of GL object handles.
+/// @ingroup gl_api_wrap
+/// @see al_object_name_view
+/// @see al_object_name_array
+template <typename Tag>
+using al_object_name_span = c_api::basic_handle_span<al_object_name<Tag>>;
+
+/// @brief Alias for template wrapping a const span of GL object handles.
+/// @ingroup gl_api_wrap
+/// @see al_object_name_span
+/// @see al_object_name_array
+template <typename Tag>
+using al_object_name_view = c_api::basic_handle_view<al_object_name<Tag>>;
+
+/// @brief Alias for template wrapping an array of GL object handles.
+/// @ingroup gl_api_wrap
+/// @see al_object_name_vector
+/// @see al_object_name_span
+/// @see al_object_name_view
+template <typename Tag, std::size_t N>
+using al_object_name_array = c_api::basic_handle_array<al_object_name<Tag>, N>;
+
+/// @brief Alias for template wrapping a vector of GL object handles.
+/// @ingroup gl_api_wrap
+/// @see al_object_name_array
+/// @see al_object_name_span
+/// @see al_object_name_view
+template <typename Tag>
+using al_object_name_vector = c_api::basic_handle_vector<al_object_name<Tag>>;
 //------------------------------------------------------------------------------
 using source_tag = EAGINE_MSG_TYPE(al, Source);
 using buffer_tag = EAGINE_MSG_TYPE(al, Buffer);

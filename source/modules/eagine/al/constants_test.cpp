18a5a239cdb6c74d20f9efc1d93e5fbaef0bcf38
Matus Chochlik <chochlik@gmail.com>
2023-02-19 05:13:05 +0100

    Added tests for constant conversions

---
+++ source/modules/eagine/al/constants_test.cpp
@@ -0,0 +1,41 @@
+/// @file
+///
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
+
+#include <eagine/testing/unit_begin.hpp>
+import eagine.core;
+import eagine.oalplus.al;
+import <set>;
+//------------------------------------------------------------------------------
+void constants_enum_by_name(auto& s) {
+    eagitest::case_ test{s, 1, "AL enum by name"};
+    std::set<eagine::oalplus::al_types::enum_type> distinct;
+
+    const auto check{[&](eagine::string_view name) {
+        if(auto value{eagine::oalplus::al_enum_by_name(name)}) {
+            distinct.insert(extract(value));
+        } else {
+            test.fail(name);
+        }
+    }};
+    check("inverse_distance");
+    check("inverse_distance_clamped");
+    check("linear_distance");
+    check("linear_distance_clamped");
+    check("exponent_distance");
+    check("exponent_distance_clamped");
+
+    test.check_equal(distinct.size(), 6U, "constant count");
+}
+//------------------------------------------------------------------------------
+auto main(int argc, const char** argv) -> int {
+    eagitest::suite test{argc, argv, "constants", 1};
+    test.once(constants_enum_by_name);
+    return test.exit_code();
+}
+//------------------------------------------------------------------------------
+#include <eagine/testing/unit_end.hpp>

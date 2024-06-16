800ac1f4c635e8f9178aab16abfdd0b5e0ec2506
Matus Chochlik <chochlik@gmail.com>
2024-06-16 11:39:34 +0200

    Added the examples install component + updated sub-modules

--- config/EAGinePackageOALplus.cmake
+++ config/EAGinePackageOALplus.cmake
@@ -6,9 +6,12 @@
 # Package specific options
 #  Debian
 #   Dependencies
-set(CPACK_DEBIAN_OALPLUS-APPS_PACKAGE_DEPENDS "")
+set(CXX_RUNTIME_PKGS "libc6,libc++1-17")
+set(CPACK_DEBIAN_OALPLUS-APPS_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
+set(CPACK_DEBIAN_OALPLUS-EXAMPLES_PACKAGE_DEPENDS "${CXX_RUNTIME_PKGS},libsystemd0,zlib1g")
 set(CPACK_DEBIAN_OALPLUS-DEV_PACKAGE_DEPENDS "eagine-core-dev (>= @EAGINE_VERSION@)")
 #   Descriptions
 set(CPACK_DEBIAN_OALPLUS-APPS_DESCRIPTION "Collection of OALplus applications.")
+set(CPACK_DEBIAN_OALPLUS-EXAMPLESS_DESCRIPTION "EAGine OALplus examples.")
 set(CPACK_DEBIAN_OALPLUS-DEV_DESCRIPTION "C++ wrapper for OpenAL and ALUT.")
 

a891733d9317dc49a028f13777ca8c514346e460
Matus Chochlik <chochlik@gmail.com>
2024-06-19 11:58:48 +0200

    Typo fix in the build system

--- config/EAGinePackageOALplus.cmake
+++ config/EAGinePackageOALplus.cmake
@@ -3,4 +3,4 @@
 #  See accompanying file LICENSE_1_0.txt or copy at
 #  https://www.boost.org/LICENSE_1_0.txt
 file(APPEND "${CMAKE_BINARY_DIR}/CPackPropertiesEAGine.cmake"
-	"set(EAGINE_OALPLUS_RUNTIME_PKGS \"libopenal1,libalut0"\")\n")
+	"set(EAGINE_OALPLUS_RUNTIME_PKGS \"libopenal1,libalut0\")\n")
